886a11c3e86f5da14da36e29bafae1abf2ae46f3
AnthonyGreco20 <9agreco@stpatrick.org>
2017-03-23 13:54:13 -0500

    Finished SameColor and Different Color

--- click.js
+++ click.js
@@ -1,16 +1,28 @@
 //declare samecolor() here
 function samecolor(){
     const doc = document;
-    let div = doc.querySelectorAll("#samecolor div");
-    div[0].style.backgroundColor=green;
-    div[1].style.backgroundColor=green;
-    div[2].style.backgroundColor=green;
-    div[3].style.backgroundColor=green;
-}
+    let color= "aqua";
+    let divs = doc.querySelectorAll("#samecolor div");
 
-//declare diffcolor() here
-function diffcolor(hex){
-    const doc = document;
-    let div= doc.querySelectorAll("#diffcolor");
-    div.style.backgroundColor=
+    divs[0].style.backgroundColor="purple";
+    divs[1].style.backgroundColor="purple";
+    divs[2].style.backgroundColor="purple";
+    divs[3].style.backgroundColor="purple";
+    divs[0].style.opacity=0.3;
+    divs[1].style.opacity=0.5;
+    divs[2].style.opacity=0.7;
+    divs[3].style.opacity=0.9;
+}
+function diffcolor(){
+    let doc=document;
+    let divs2=doc.querySelectorAll("#diffcolor div")
+    divs2[0].style.backgroundColor="gold";
+    divs2[1].style.backgroundColor="green";
+    divs2[2].style.backgroundColor="gold";
+    divs2[3].style.backgroundColor="green";
+    divs2[4].style.backgroundColor="gold";
+    divs2[5].style.backgroundColor="green";
+    divs2[6].style.backgroundColor="gold";
+    divs2[7].style.backgroundColor="green";
+
 }
